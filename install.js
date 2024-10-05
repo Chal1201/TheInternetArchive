addcdbc9bd43fabe2e8b519d9bb6ce47927c0273
Chal1201 <Charlieschofield16@outlook.com>
2024-10-05 15:04:42 +0100

    Update install.js

--- install.js
+++ install.js
@@ -1,9 +1,9 @@
 document.getElementById('downloadXP').addEventListener('click', function() {
-    window.location.href = '/iso/en_windows_xp_professional_with_service_pack_3_x86_cd_vl_x14-73974.iso';
+    window.location.href = '/en_windows_xp_professional_with_service_pack_3_x86_cd_vl_x14-73974.iso';
 });
 
 document.getElementById('downloadPi').addEventListener('click', function() {
-    window.location.href = '/iso/2022-07-01-raspios-bullseye-i386.iso';
+    window.location.href = '/2022-07-01-raspios-bullseye-i386.iso';
 });
 
 window.onload = function() {
