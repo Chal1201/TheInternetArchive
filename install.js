0f671b79f72a013d276da1596ef16eaad673690c
Chal1201 <Charlieschofield16@outlook.com>
2024-10-05 15:01:33 +0100

    Add files via upload

---
+++ install.js
@@ -0,0 +1,11 @@
+document.getElementById('downloadXP').addEventListener('click', function() {
+    window.location.href = '/iso/en_windows_xp_professional_with_service_pack_3_x86_cd_vl_x14-73974.iso';
+});
+
+document.getElementById('downloadPi').addEventListener('click', function() {
+    window.location.href = '/iso/2022-07-01-raspios-bullseye-i386.iso';
+});
+
+window.onload = function() {
+    document.getElementById('startSound').play();
+};
